16604ba49b944fe0c3b04aecca6e34f5681ce7f8
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-01 17:56:12 +0200

    qweqw

--- app/Filament/Office/Pages/Dashboard.php
+++ app/Filament/Office/Pages/Dashboard.php
@@ -29,7 +29,7 @@ class Dashboard extends BaseDashboard
         ];
     }
 
-    public function getColumns(): int | string | array
+    public function getColumns(): int | array
     {
         return 2;
     }
