a2de0fdb09e8ac99e3611b0e53af6a76187e52c9
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-01 17:26:46 +0200

    qweqw

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -93,7 +93,6 @@ class AdminPanelProvider extends PanelProvider
             ->navigationGroups([
                 'الإدارة',
                 'النظام',
-            ])
-            ->databaseNotifications();
+            ]);
     }
 }
