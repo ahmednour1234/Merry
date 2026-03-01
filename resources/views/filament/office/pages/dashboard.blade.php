5e6af42b36d1d07c92377b4c6b0022e6eb34aeac
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-02 00:40:51 +0200

    qweqw

--- resources/views/filament/office/pages/dashboard.blade.php
+++ resources/views/filament/office/pages/dashboard.blade.php
@@ -1,6 +1,14 @@
 <x-filament-panels::page>
-    <x-filament-panels::widgets
-        :widgets="$this->getWidgets()"
-        :columns="$this->getColumns()"
-    />
+    @php
+        $widgets = $this->getWidgets();
+        $columns = $this->getColumns();
+    @endphp
+    
+    @if(!empty($widgets))
+        <div class="fi-widgets-grid grid gap-6 {{ is_array($columns) ? '' : 'grid-cols-1 md:grid-cols-' . $columns }}">
+            @foreach($widgets as $widget)
+                @livewire($widget)
+            @endforeach
+        </div>
+    @endif
 </x-filament-panels::page>
