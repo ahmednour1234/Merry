f71a9a081149c31dbf0d0d8c8c2c1816b4430d7b
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-01 18:38:10 +0200

    qweqw

--- app/Providers/Filament/OfficePanelProvider.php
+++ app/Providers/Filament/OfficePanelProvider.php
@@ -27,7 +27,6 @@ class OfficePanelProvider extends PanelProvider
             ->path('office')
             ->registration()
             ->login()
-            ->rtl()
             ->brandName('تطبيق ميري - المكتب')
             ->colors([
                 'primary' => Color::hex('#054F31'),
