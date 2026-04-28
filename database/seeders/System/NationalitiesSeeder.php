8966f3d65d76836dbe8642ca6c177632cecf3f8c
Ahmed Nour <106186787+ahmednour1234@users.noreply.github.com>
2026-04-28 17:54:15 +0300

    wqe

--- database/seeders/System/NationalitiesSeeder.php
+++ database/seeders/System/NationalitiesSeeder.php
@@ -12,10 +12,12 @@ class NationalitiesSeeder extends Seeder
         $db = DB::connection('system');
 
         $items = [
-            ['code'=>'SA','name'=>'Saudi','active'=>1, 'trans'=>[['lang_code'=>'ar','name'=>'سعودي']]],
-            ['code'=>'EG','name'=>'Egyptian','active'=>1, 'trans'=>[['lang_code'=>'ar','name'=>'مصري']]],
-            ['code'=>'PH','name'=>'Filipino','active'=>1, 'trans'=>[['lang_code'=>'ar','name'=>'فلبيني']]],
-            ['code'=>'IN','name'=>'Indian','active'=>1, 'trans'=>[['lang_code'=>'ar','name'=>'هندي']]],
+            ['code'=>'PH','name'=>'Filipino',   'image'=>'https://flagcdn.com/w40/ph.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'فلبيني']]],
+            ['code'=>'ET','name'=>'Ethiopian',  'image'=>'https://flagcdn.com/w40/et.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'إثيوبي']]],
+            ['code'=>'BD','name'=>'Bangladeshi','image'=>'https://flagcdn.com/w40/bd.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'بنغلاديشي']]],
+            ['code'=>'LK','name'=>'Sri Lankan', 'image'=>'https://flagcdn.com/w40/lk.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'سريلانكي']]],
+            ['code'=>'UG','name'=>'Ugandan',    'image'=>'https://flagcdn.com/w40/ug.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'أوغندي']]],
+            ['code'=>'BI','name'=>'Burundian',  'image'=>'https://flagcdn.com/w40/bi.png','active'=>1,'trans'=>[['lang_code'=>'ar','name'=>'بوروندي']]],
         ];
 
         foreach ($items as $it) {
@@ -23,6 +25,7 @@ class NationalitiesSeeder extends Seeder
                 ['code'=>$it['code']],
                 [
                     'name'=>$it['name'],
+                    'image'=>$it['image'],
                     'active'=>$it['active'],
                     'meta'=>json_encode([]),
                     'created_at'=>now(),
